0bb0c313384d02411d92c122f86d4c320d10ac60
Marc Oliva Vallabriga <marcoliva96@gmail.com>
2017-03-09 22:52:06 +0100

    function hourly implemented ok. print_hourly pending

--- treball.py
+++ treball.py
@@ -36,33 +36,32 @@ class WeatherClient(object):
     def hourly(self, location):
         """Saves 'hourly' data."""
         data = self.requestData("hourly")
+        jsondata = json.loads(data)["hourly_forecast"]  # all the data
 
-        jsondata = json.loads(data)["hourly_forecast"]
-        for date in jsondata:
-            print location + ', ' + date["FCTTIME"]["pretty"]
-            print "Temperature: " + date["temp"]["metric"] + \
-                " (ºC)".decode("utf-8")
-            print "Condition: " + date["condition"]
-            print "Wind speed: " + date["wspd"]["metric"] + " (Km/h)"
-            print "Humidity: " + date["humidity"] + " (%)"
-            print "Pressure: " + date["mslp"]["metric"] + " (hPa)" + '\n'
-
-        return jsondata
+        useful_data = []  # just useful data for us
+        hourly_data = {}
+        for hour in jsondata:
+                hourly_data["location"] = hour["FCTTIME"]["pretty"]
+                hourly_data["temp"] = hour["temp"]["metric"]
+                hourly_data["condition"] = hour["condition"]
+                hourly_data["wind"] = hour["wspd"]["metric"]
+                hourly_data["humidity"] = hour["humidity"]
+                hourly_data["pressure"] = hour["mslp"]["metric"]
+                useful_data.append(hourly_data.copy())
+        return useful_data
 
     def almanac(self, location):
         """Saves 'almanac' data."""
         data = self.requestData("almanac")
-
-        jsondata = json.loads(data)
-        almanac = jsondata["almanac"]  # all the data
+        jsondata = json.loads(data)["almanac"]  # all the data
 
         useful_data = {}  # just useful data for us
         useful_data["max"] = {}
         useful_data["min"] = {}
-        useful_data["max"]["avg"] = almanac["temp_high"]["normal"]["C"]
-        useful_data["max"]["record"] = almanac["temp_high"]["record"]["C"]
-        useful_data["min"]["avg"] = almanac["temp_low"]["normal"]["C"]
-        useful_data["min"]["record"] = almanac["temp_low"]["record"]["C"]
+        useful_data["max"]["avg"] = jsondata["temp_high"]["normal"]["C"]
+        useful_data["max"]["record"] = jsondata["temp_high"]["record"]["C"]
+        useful_data["min"]["avg"] = jsondata["temp_low"]["normal"]["C"]
+        useful_data["min"]["record"] = jsondata["temp_low"]["record"]["C"]
 
         return useful_data
 
@@ -78,6 +77,19 @@ def print_almanac(useful_data):
     print "(Lowest ever", useful_data["min"]["record"] + ")"
 
 
+def print_hourly(useful_data):
+    """Prints saved data as a dict"""
+    #
+    for data in useful_data:
+        print location + ', ' + useful_data["location"]
+        print "Temperature: " + useful_data["temp"] + \
+            " (ºC)".decode("utf-8")
+        print "Condition: " + useful_data["condition"]
+        print "Wind speed: " + useful_data["wind"] + " (Km/h)"
+        print "Humidity: " + useful_data["humidity"] + " (%)"
+        print "Pressure: " + useful_data["pressure"] + " (hPa)" + '\n'
+
+
 if __name__ == "__main__":
     if not api_key:
         try:
@@ -89,3 +101,4 @@ if __name__ == "__main__":
 
     weatherclient = WeatherClient(api_key)
 print_almanac(weatherclient.almanac(location))
+print weatherclient.hourly(location)
